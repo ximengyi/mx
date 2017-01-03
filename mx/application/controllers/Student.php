29873c2ae4cadfcfb7f82438ec8b978647ce230d
ximengyi <1203669692@qq.com>
2017-01-03 19:50:21 +0800

    完成了添加学员信息和查询学员信息和列表功能

--- mx/application/controllers/Student.php
+++ mx/application/controllers/Student.php
@@ -3,57 +3,91 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Student extends CI_Controller {
 
-	
+	   public function __construct(){
+			 parent::__construct();
+			 $this->load->library('pagination');
+    	 $this->load->model('Stu_model');
+		 }
+
+
 	public function index()
-	{ 
+	{
 		$this->load->view('header.html');
 		$this->load->view('index.html');
 		$this->load->view('footer.html');
 	}
+
+
+
+
 	public function insertStu()
 	{
 		$this->load->helper('form');
 		$this->load->library('form_validation');
-		$this->load->view('header.html');
+		$this->form_validation->set_rules('stuname', '姓名', 'required');
+		$this->form_validation->set_rules('sex', '性别', 'required');
+		$this->form_validation->set_rules('birthday', '出生年月', 'required');
+		$this->form_validation->set_rules('parentname', '家长姓名', 'required');
+		$this->form_validation->set_rules('phone', '联系电话', 'required');
+		$this->form_validation->set_rules('addreess', '家庭住址', 'required');
+		$this->form_validation->set_rules('rtext', '备注信息', 'required');
+		$status = $this->form_validation->run();
+
+		$this->load->view('header.html');  //载入视图
 		$this->load->view('insertStu.html');
 		$this->load->view('footer.html');
 
-
-		$stuname = $this->input->post('stuname');
+		$stuname = $this->input->post('stuname');//获取表单数据
 		$sex =$this->input->post('sex');
 		$birthday = $this->input->post('birthday');
 		$parentname = $this->input->post('parentname');
 		$phone = $this->input->post('phone');
 		$addreess = $this->input->post('addreess');
-        $rtext =$this->input->post('rtext');
-    
-    //表单数据
-         $studata = array(
-         	'name' =>$stuname ,
+		$rtext =$this->input->post('rtext');
+
+
+       $studata = array(
+      'name' =>$stuname ,
 			'sex' =>$sex ,
 			'birthday' =>$birthday ,
 			'parentname' =>$parentname ,
 			'phone' =>$phone ,
-			'addreess' =>$addreess ,
-			'remarks' =>$rtext 
+			'adreess' =>$addreess ,
+			'remarks' =>$rtext
 	);
-       foreach ($studata as $key ) {
-       	echo $key;
-       }
-    
-        	//表单验证
-  		$this->form_validation->set_rules('stuname', '姓名', 'required');
-        $this->form_validation->set_rules('sex', '性别', 'required');
-  		$this->form_validation->set_rules('birthday', '出生年月', 'required');
-        $this->form_validation->set_rules('parentname', '家长姓名', 'required');
-  		$this->form_validation->set_rules('phone', '联系电话', 'required');
-        $this->form_validation->set_rules('addreess', '家庭住址', 'required');
-  		$this->form_validation->set_rules('rtext', '备注信息', 'required');
+
+				if ($status)
+				{
+          $this->Stu_model->ins_stu($studata);
+           success('Student/baseMessage','添加成功');
+				}
 
 
-
-		
 	}
+
+	// public function doinsertstu(){
+	// 	$this->load->helper('form');
+	// 	$this->load->library('form_validation');
+	//
+	// 	$this->form_validation->set_rules('stuname', '姓名', 'required');
+	// 	// $this->form_validation->set_rules('sex', '性别', 'required');
+	// 	// $this->form_validation->set_rules('birthday', '出生年月', 'required');
+	// 	// $this->form_validation->set_rules('parentname', '家长姓名', 'required');
+	// 	// $this->form_validation->set_rules('phone', '联系电话', 'required');
+	// 	// $this->form_validation->set_rules('addreess', '家庭住址', 'required');
+	// 	// $this->form_validation->set_rules('rtext', '备注信息', 'required');
+	// 	$status = $this->form_validation->run();
+  //   echo $status;
+	//
+	// 	if ($status) {
+	//    echo "123";
+	//  }else{
+	// 	 echo"456";
+	//  }
+	//
+	//
+	// }
+
 	public function findStu()
 	{
 		$this->load->view('header.html');
@@ -65,20 +99,43 @@ class Student extends CI_Controller {
 		$this->load->view('header.html');
 		$this->load->view('newCourse.html');
 		$this->load->view('footer.html');
-		
+
 	}
 	public function stuAddCourse()
 	{
 		$this->load->view('header.html');
 		$this->load->view('stuAddCourse.html');
 		$this->load->view('footer.html');
-		
+
 	}
 
 	public function baseMessage(){
-	    $this->load->view('header.html');
-		$this->load->view('catstu.html');
-		$this->load->view('footer.html');
+		    //   $data = $this->Stu_model->sturecord();
+				// 	echo "<pre>";
+				// var_dump($data);
+				//   echo "</pre>";
+				$this->load->library('pagination');
+		    $perPage =8;
+	    	$config['base_url'] = site_url('Student/baseMessage');
+	      $config['total_rows']= $this->db->count_all_results('student');
+	      $config['per_page'] = $perPage;
+				$config['uri_segment'] = 3;
+	      $config['first_link'] = '第一页';
+	      $config['prev_link'] = '上一页';
+	      $config['next_link'] = '下一页';
+	      $config['last_link'] = '最后一页';
+				$this->pagination->initialize($config);
+				$data['links'] = $this->pagination->create_links();
+				$offset = $this->uri->segment(3);
+				$this->db->limit($perPage,$offset);
+
+				$data['stuMessage'] = $this->Stu_model->sturecord();
+
+
+				$this->load->view('header.html');
+				$this->load->view('catstu.html',$data);
+				$this->load->view('footer.html');
+
 	}
 
 	public function addCourse(){
@@ -87,5 +144,5 @@ class Student extends CI_Controller {
 		$this->load->view('footer.html');
 
 	}
-	
+
 }
--- mx/application/models/Stu_model.php
+++ mx/application/models/Stu_model.php
@@ -13,9 +13,17 @@ class Stu_model extends CI_Model{
 
      	$this->db->insert('student',$data);
 
-     }    
+		}
+
+		public function sturecord(){
+		//$data = $this->db->select('record_id,moocid,type,title,grade,name,state,approver,time')->from('record')->order_by('record_id','desc')->get()->result_array();
+	  	//$array = array('moocid'=>$moocid,'state' => '未通过');
+		//	$data = $this->db->where($array)->from('record')->order_by('record_id','desc')->get()->result_array();
+			    $data = $this->db->select()->from('student')->get()->result_array();
+
+				 //	  $query = $this->db->('student');
+		      	return  $data;
+
+	 }
 
     }
-
-
-}
